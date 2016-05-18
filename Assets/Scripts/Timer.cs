1ad8582cbe6e1c4a3b203b15c24c62abe25fc68f
Tim <tbajmat@gmail.com>
2016-05-18 19:33:30 +0200

    Added Screens + transitions
    Added fading and timers

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	float timeLeft = 80f;
+
+	public delegate void TimerStop();
+	public static event TimerStop OnTimerStop;
+
+	void Update()
+	{
+		timeLeft -= Time.deltaTime;
+		if(timeLeft < 0)
+		{
+			if(OnTimerStop != null)
+			{
+				print("Fade should start");
+				OnTimerStop();
+			}
+		}
+
+		print(timeLeft);
+	}
+}
